3b272d5dfd77c5eb5c436afb7351db37b5e9a763
Razzaq <113843260+razzaqali778@users.noreply.github.com>
2023-06-14 09:15:03 +0530

    Add files via upload

---
+++ dsa_concepts_js/sorting/bubbleSort.js
@@ -0,0 +1,17 @@
+const bubbleSort = (arr) => {
+  const len = arr.length;
+
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+};
+
+const array = [5, 3, 8, 4, 2];
+const sortedArray = bubbleSort(array);
+console.log(sortedArray);
