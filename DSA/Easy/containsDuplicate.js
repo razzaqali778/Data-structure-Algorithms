b15cc13360d90f7285f333d543792d5221277f3b
Razzaq <113843260+razzaqali778@users.noreply.github.com>
2023-08-23 20:41:04 +0530

    Create containsDuplicate.js

---
+++ DSA/Easy/containsDuplicate.js
@@ -0,0 +1,24 @@
+// sudo
+
+var containsDuplicate = (nums) => {
+    for (let right = 0; right < nums.length; right++) {/* Time O(N) */
+        for (let left = 0; left < right; left++) {         /* Time O(N) */
+            const isDuplicate = nums[left] === nums[right];
+            if (isDuplicate) return true;
+        }
+    }
+
+    return false;
+}
+
+// using sets
+
+var containsDuplicate = (nums, numsSet = new Set()) => {
+    for (const num of nums) {/* Time O(N) */
+        if (numsSet.has(num)) return true;
+
+        numsSet.add(num);       /* Space O(N) */
+    }
+
+    return false;
+};
