9d8055b958b115e1d8bf8ea5d02e558cdf9a371f
Razzaq <113843260+razzaqali778@users.noreply.github.com>
2023-08-24 20:57:43 +0530

    Create lengthOfLastWord.js

---
+++ DSA/Easy/lengthOfLastWord.js
@@ -0,0 +1,18 @@
+//58
+
+var lengthOfLastWord = function(s) {
+
+    let firstCharOccurance = false;
+    let lastWordLen = 0;
+
+    for(let i = s.length - 1; i > -1; i--) {
+        if(s[i] !== ' ') {
+            firstCharOccurance = true;
+            lastWordLen++;
+        }
+        if(firstCharOccurance && s[i] === ' ') {
+            break;
+        }
+    }
+    return lastWordLen;
+};
