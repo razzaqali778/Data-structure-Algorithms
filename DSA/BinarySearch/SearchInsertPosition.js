03d76887846ae55fa36f8e65596e35a78c1487a6
Razzaq <113843260+razzaqali778@users.noreply.github.com>
2023-09-06 21:07:33 +0530

    Create SearchInsertPosition.js

---
+++ DSA/BinarySearch/SearchInsertPosition.js
@@ -0,0 +1,20 @@
+//35
+
+var searchInsert = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let midIdx = Math.floor((left + right) / 2);
+        if (target === nums[midIdx]) {
+            return midIdx;
+        }
+
+        if (target > nums[midIdx]) {
+            left = midIdx + 1;
+        } else {
+            right = midIdx - 1;
+        }
+    }
+
+    return left;
+};
