c93a892644102b2cadc6b300acfdc500ffaf0584
Razzaq <113843260+razzaqali778@users.noreply.github.com>
2023-09-12 21:19:00 +0530

    Create Best Time to Buy and Sell Stock.js

---
+++ DSA/Easy/Best Time to Buy and Sell Stock.js
@@ -0,0 +1,20 @@
+//121
+
+function maxProfit(prices) {
+
+  if (prices.length <= 1) {
+    return 0;
+  }
+
+  let minPrice = prices[0];
+  let maxProfit = 0;
+
+  for (let i = 0; i < prices.length; i++) {
+    minPrice = Math.min(minPrice, prices[i]);
+    maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+  }
+
+  return maxProfit;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
