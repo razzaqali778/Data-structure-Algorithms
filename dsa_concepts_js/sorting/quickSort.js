787f9cb53910bda3bf739675b04eaf0c7e7179fa
Razzaq <113843260+razzaqali778@users.noreply.github.com>
2023-09-12 20:54:34 +0530

    Update quickSort.js

--- dsa_concepts_js/sorting/quickSort.js
+++ dsa_concepts_js/sorting/quickSort.js
@@ -1,3 +1,44 @@
+
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+};
+
+
+const unsortedArray = [5, 2, 9, 1, 5, 6];
+const sortedArray = quickSort(unsortedArray);
+console.log(sortedArray); // Output: [1, 2, 5, 5, 6, 9]
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 const quickSort = (arr, low = 0, high = arr.length - 1) => {
   if (low < high) {
     const pivotIndex = partition(arr, low, high);
@@ -26,3 +67,20 @@ const partition = (arr, low, high) => {
 const array = [5, 3, 8, 4, 2];
 const sortedArray = quickSort(array);
 console.log(sortedArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
