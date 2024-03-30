ffe77738d8eadbf4555136069894ebfad6e4ed2a
Razzaq <113843260+razzaqali778@users.noreply.github.com>
2024-03-30 11:00:03 +0530

    Update reverseWithoutanyMethod.js

--- DSA/Numbers/reverseWithoutanyMethod.js
+++ DSA/Numbers/reverseWithoutanyMethod.js
@@ -1,4 +1,48 @@
 
+ function reverseNumberWithLeadingZeros(number) {
+    let reversedNumber = ''
+    let isNegative = number < 0
+
+    number = Math.abs(number)
+
+    while (number > 0 || reversedNumber.length === 0) {
+      let lastDigit = number % 10
+      reversedNumber += lastDigit
+      number = (number - lastDigit) / 10
+
+      if (number === 0 && reversedNumber.length === 0) {
+        reversedNumber += '0'
+      }
+    }
+
+    if (isNegative) {
+      reversedNumber = '-' + reversedNumber
+    }
+
+    return parseInt(reversedNumber)
+  }
+
+  const value = reverseNumberWithLeadingZeros(123450)
+
+  console.log(typeof value) // Outputs: "054321"
+  console.log(reverseNumberWithLeadingZeros(-123450)) // Outputs: "-054321"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const reverseNumberWithoutanyMethod=(num)=>{
 let toBeReverse = '';
 
